046958cba6f487343bfb09a7b8b49d79685b6f64
Joyce Silva <142221695+joycesilvaaa@users.noreply.github.com>
2024-04-19 22:49:56 -0300

    adiconei o arquivo interface funcionario já configurado

---
+++ src/Backend/src/interfaces/IFuncionario.ts
@@ -0,0 +1,34 @@
+import Chamado from "../entities/chamado";
+
+export interface IFuncionarioView {
+    func_nome: string
+    func_cpf: string
+    func_email: string
+    func_telefone: number
+    func_senha: string
+    func_expediente_inicio: string
+    func_expediente_final: string
+    ativo: boolean
+}
+
+export interface IFuncionarioInput {
+    func_nome: string
+    func_cpf: string
+    func_email: string
+    func_telefone: number
+    func_senha: string
+    func_expediente_inicio: string
+    func_expediente_final: string
+    func_is_admin: boolean
+}
+
+export interface IFuncionarioUpdate{
+    func_nome?: string
+    func_cpf?: string
+    func_email?: string
+    func_telefone?: number
+    func_senha?: string
+    func_expediente_inicio?: string
+    func_expediente_final?: string
+    ativo?: boolean
+}
