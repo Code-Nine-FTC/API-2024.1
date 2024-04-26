949b0d0b190087c511b4d805bab52ca501576004
Júlia Rosado <julia.rosado@fatec.sp.gov.br>
2024-04-26 14:13:50 -0300

    feat
    Adicionei o controller do funcionário já configurado

---
+++ src/Backend/src/controllers/funcionarioController.ts
@@ -0,0 +1,77 @@
+import { Response, Request } from "express";
+import { FuncionarioService } from "../services/funcionarioService";
+import { IFuncionarioInput, IFuncionarioUpdate, IFuncionarioLoggin } from "../interfaces/IFuncionario";
+
+export default class FuncionarioController {
+    private funcionarioService: FuncionarioService
+    constructor() {
+        this.funcionarioService = new FuncionarioService()
+    }
+    async cadastrarFuncionario(req: Request, res: Response) {
+        try {
+            const dadosFuncionario: IFuncionarioInput = req.body;
+            const resultado = await this.funcionarioService.cadastrarFuncionario(dadosFuncionario);
+            if (resultado.success) {
+                return res.status(200).json(resultado);
+            } else {
+                return res.status(400).json(resultado);
+            }
+        } catch (error) {
+            console.error(`Erro no cadastro do funcionário: ${error}`);
+            return res.status(500).json({ success: false, message: `Erro interno do servidor` });
+        }
+    }
+    
+    async editarFuncionario(req: Request, res: Response) {
+        try {
+            const id = Number(req.params.id)
+            if (isNaN(id) || id <= 0) {
+                return res.status(400).json({ success: false, message: 'ID do funcionário inválido' });
+            }
+            const dadosUpdate: IFuncionarioUpdate = req.body
+            const resultado = await this.funcionarioService.editarFuncionario(id, dadosUpdate)
+            if (resultado.success) {
+                return res.status(200).json(resultado)
+            } else {
+                return res.status(400).json(resultado)
+            }
+        } catch (error) {
+            console.error(`Erro em editar o funcionário: ${error}`)
+            return res.status(500).json({ success: false, message: `Erro interno do servidor` })
+        }
+    }
+
+    async visualizarFuncionario(req: Request, res: Response) {
+        try {
+            const id: number = Number(req.params.id)
+            if (isNaN(id) || id <= 0) {
+                return res.status(400).json({ success: false, message: 'ID do funcionário inválido' });
+            }
+            const resultado = await this.funcionarioService.visualizarFuncionario(id)
+            if (resultado.success) {
+                return res.status(200).json(resultado)
+            } else {
+                return res.status(400).json(resultado)
+            }
+        } catch (error) {
+            console.error(`Erro em visualizar o funcionário: ${error}`)
+            return res.status(500).json({ success: false, message: `Erro interno do servidor` })
+        }
+    }
+    
+    async logginFuncionario(req: Request, res: Response) {
+        try {
+            const dadosLoggin: IFuncionarioLoggin = req.body;
+            const resultado = await this.funcionarioService.logginFuncionario(dadosLoggin);
+    
+            if (resultado.success) {
+                return res.status(200).json(resultado);
+            } else {
+                return res.status(400).json(resultado);
+            }
+        } catch (error) {
+            console.error(`Erro no login do funcionário: ${error}`);
+            return res.status(500).json({ success: false, message: 'Erro interno do servidor' });
+        }
+    }
+}
