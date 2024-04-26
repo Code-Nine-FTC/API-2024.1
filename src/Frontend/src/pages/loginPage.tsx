e2cccaf3d252333416d664f01ed5ba8dc039a707
Jonasoliver <142221848+Jonasoliver@users.noreply.github.com>
2024-04-26 12:24:39 -0300

    tela de login
    subi a tela de login, adicionei o css dela tambem

---
+++ src/Frontend/src/pages/loginPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginForm from '../component/login/formLogin';
+
+function LoginPage() {
+  return (
+    <LoginForm/>
+
+  );
+}
+
+
+
+
+export default LoginPage;
