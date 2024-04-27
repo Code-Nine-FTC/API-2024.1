52a9684b674ff1bfd8798248193bffe3f1ce3ea6
Joyce Silva <142221695+joycesilvaaa@users.noreply.github.com>
2024-04-27 11:28:08 -0300

    feat:  Inserir Administrador

--- src/Backend/src/app.ts
+++ src/Backend/src/app.ts
@@ -6,6 +6,8 @@ import dotenv from 'dotenv';
 import path from 'path';
 import clienteRoutes from './routes/clienteRoutes'
 import funcionarioRoutes from './routes/funcionarioRoutes'
+import Funcionario from './entities/funcionario';
+import * as bcrypt from 'bcrypt';
 
 // Carrega as variáveis de ambiente do arquivo .env
 dotenv.config({ path: path.resolve(__dirname, '.env') })
@@ -19,10 +21,33 @@ app.use(clienteRoutes)
 app.use(funcionarioRoutes)
 
 //inicializa o banco de dados e da start se estiver ok!
-Connection.initialize().then(async () => {
+Connection.initialize().then(() => {
     console.log(`Conexão com o banco de dados bem sucedida! :)`)
+    insertAdmin().catch(error => {
+        console.log(`Erro ao inserir administrador: `,error)
+    })
     const port = process.env.PORT || 3000
     app.listen(port, ()=> {
         console.log(`Servido rodando na porta ${port}`)
     })
-})
+}).catch(error => {
+    console.error("Erro ao inicializar conexão com o banco de dados:", error);
+});
+async function insertAdmin(){
+        const funcionarioRepository = await Connection.getRepository(Funcionario)
+        const count = await funcionarioRepository.count()
+        const admin = await funcionarioRepository.findOne({where: {func_is_admin: true}})
+        if (count === 0 || !admin)  {
+            const administrador = new Funcionario()
+            administrador.func_nome ='CodeNine'
+            administrador.func_cpf ='41094483877'
+            administrador.func_email='nine.codek9@gmail.com'
+            administrador.func_expediente_inicio ='00:00:00'
+            administrador.func_expediente_final ='00:00:00'
+            administrador.func_is_admin =true
+            let senha = bcrypt.hash('CodeNine', 10)
+            administrador.func_senha = senha
+            await funcionarioRepository.save(administrador)
+            console.log(`Administrador inserido com sucesso`)
+        }    
+    }
