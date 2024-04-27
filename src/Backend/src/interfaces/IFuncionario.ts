e73859c43c5db0c28eab31edfcfde5c54ecf51c8
Joyce Silva <142221695+joycesilvaaa@users.noreply.github.com>
2024-04-27 11:35:06 -0300

    refactor: removi func_telefone

--- src/Backend/src/interfaces/IFuncionario.ts
+++ src/Backend/src/interfaces/IFuncionario.ts
@@ -1,10 +1,7 @@
-import Chamado from "../entities/chamado";
-
 export interface IFuncionarioView {
     func_nome: string
     func_cpf: string
     func_email: string
-    func_telefone: number
     func_senha: string
     func_expediente_inicio: string
     func_expediente_final: string
@@ -15,7 +12,6 @@ export interface IFuncionarioInput {
     func_nome: string
     func_cpf: string
     func_email: string
-    func_telefone: number
     func_senha: string
     func_expediente_inicio: string
     func_expediente_final: string
@@ -31,7 +27,6 @@ export interface IFuncionarioUpdate{
     func_nome?: string
     func_cpf?: string
     func_email?: string
-    func_telefone?: number
     func_senha?: string
     func_expediente_inicio?: string
     func_expediente_final?: string
