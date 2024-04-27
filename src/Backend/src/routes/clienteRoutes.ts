80cf65bf81ad332621f78238b3a1b6ea120d5aa3
Joyce Silva <142221695+joycesilvaaa@users.noreply.github.com>
2024-04-27 11:36:49 -0300

    refactor: corrigi método, feat: rota desativar cliente

--- src/Backend/src/routes/clienteRoutes.ts
+++ src/Backend/src/routes/clienteRoutes.ts
@@ -6,7 +6,8 @@ const clienteController = new ClienteController()
 
 router.post('/cadastroCliente', clienteController.cadastrarCliente.bind(clienteController))
 router.post('/logginCliente', clienteController.logginCliente.bind(clienteController))
-router.get('/updateCliente/:id', clienteController.editarCliente.bind(clienteController))
+router.put('/updateCliente/:id', clienteController.editarCliente.bind(clienteController))
 router.get('/viewCliente/:id', clienteController.visualizarCliente.bind(clienteController))
+router.get('/desativarCliente/:id', clienteController.desativarCliente.bind(clienteController))
 
 export default router
