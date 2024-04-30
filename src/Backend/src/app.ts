f6db44732857ec7dc45142bd456c5ae55f6e14ff
Joyce Silva <142221695+joycesilvaaa@users.noreply.github.com>
2024-04-30 10:16:23 -0300

    fix: correção de erro da função InserirAdm

--- src/Backend/src/app.ts
+++ src/Backend/src/app.ts
@@ -8,6 +8,7 @@ import clienteRoutes from './routes/clienteRoutes'
 import funcionarioRoutes from './routes/funcionarioRoutes'
 import Funcionario from './entities/funcionario';
 import * as bcrypt from 'bcrypt';
+import { FuncionarioService } from './services/funcionarioService';
 
 // Carrega as variáveis de ambiente do arquivo .env
 dotenv.config({ path: path.resolve(__dirname, '.env') })
@@ -19,17 +20,6 @@ app.use(cors())
 app.use(express.json()) 
 app.use(clienteRoutes)
 app.use(funcionarioRoutes)
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-
-app.use((req, res, next) => {
-    console.log('Incoming request:');
-    console.log('Method:', req.method);
-    console.log('URL:', req.url);
-    console.log('Headers:', req.headers);
-    console.log('Body:', req.body);
-    next();
-});
 
 //inicializa o banco de dados e da start se estiver ok!
 Connection.initialize().then(() => {
@@ -48,17 +38,18 @@ async function insertAdmin(){
         const funcionarioRepository = await Connection.getRepository(Funcionario)
         const count = await funcionarioRepository.count()
         const admin = await funcionarioRepository.findOne({where: {func_is_admin: true}})
+        const cadastroAdministrador = new FuncionarioService()
         if (count === 0 || !admin)  {
-            const administrador = new Funcionario()
-            administrador.func_nome ='CodeNine'
-            administrador.func_cpf ='41094483877'
-            administrador.func_email='nine.codek9@gmail.com'
-            administrador.func_expediente_inicio ='00:00:00'
-            administrador.func_expediente_final ='00:00:00'
-            administrador.func_is_admin =true
-            let senha = bcrypt.hash('CodeNine', 10)
-            administrador.func_senha = senha
-            await funcionarioRepository.save(administrador)
+            const dadosFuncionario ={
+                func_nome: 'CodeNine',
+                func_cpf :'41094483877',
+                func_email :'nine.codek9@gmail.com',
+                func_senha : 'CodeNine',
+                func_expediente_inicio :'00:00:00',
+                func_expediente_final :'00:00:00',
+                func_is_admin :true
+            }
+            cadastroAdministrador.cadastrarFuncionario(dadosFuncionario)
             console.log(`Administrador inserido com sucesso`)
         }    
     }
