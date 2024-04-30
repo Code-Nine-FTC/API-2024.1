784ffe9bbafa3096307971b4422dc5fbb3e3b484
Jonasoliver <142221848+Jonasoliver@users.noreply.github.com>
2024-04-29 21:53:41 -0300

    feat: adição da tela de login do Adm
    Adicionei a tela de login do administrador

--- src/Frontend/src/component/loginadm/formAdm.tsx
+++ src/Frontend/src/component/loginadm/formAdm.tsx
@@ -2,23 +2,24 @@ import React from 'react';
 import logo from "./projeto9999.png"
 import styles from './Login.module.css'
 
-function LoginForm() {
+function LoginAdmForm() {
   return ( 
     <div className={styles.conteudo}>
       <form className={styles.conteudointerno}>
         <img src={logo} className={styles.logo} alt="logo" />
         <div className={styles.Labels}>
           <h1>Faça o seu Login</h1>
-          <label htmlFor="label1">Email</label>
-          <input type="text" id={styles.label1} placeholder="Insira seu Email" />
+          <label htmlFor="label1">CPF</label>
+          <input type="text" id={styles.label1} placeholder="Insira seu CPF" />
           <label htmlFor="label2">Senha</label>
           <input type="text" id={styles.label2} placeholder="Insira sua senha" />
           <button type="submit" className={styles.EntrarButton}>Entrar</button>
           <div className={styles.Title}>Voltar  →</div>
+          <div className={styles.Title}>É administrador?</div>
         </div>
       </form>
     </div>
   );
 }
 
-export default LoginForm;
+export default LoginAdmForm;
