00987ae850be6c3fc5d0718e4a6cb881ddf090f2
Joyce Silva <142221695+joycesilvaaa@users.noreply.github.com>
2024-04-30 22:01:26 -0300

    feat:[backend] adicionei verificação de token em rotas privadas

--- src/Backend/src/routes/clienteRoutes.ts
+++ src/Backend/src/routes/clienteRoutes.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import ClienteController from "../controllers/clienteController";
+import { Autenticacao } from "../controllers/autenticacaoMiddleware";
 
-const router = Router()
-const clienteController = new ClienteController()
+const router = Router();
+const clienteController = new ClienteController();
+const autenticacao = Autenticacao.autenticacaoToken;
 
-router.post('/cadastroCliente', clienteController.cadastrarCliente.bind(clienteController))
-router.post('/logginCliente', clienteController.logginCliente.bind(clienteController))
-router.put('/updateCliente/:id', clienteController.editarCliente.bind(clienteController))
-router.get('/viewCliente/:id', clienteController.visualizarCliente.bind(clienteController))
-router.get('/desativarCliente/:id', clienteController.desativarCliente.bind(clienteController))
+router.post('/cadastroCliente', clienteController.cadastrarCliente.bind(clienteController));
+router.post('/logginCliente', clienteController.logginCliente.bind(clienteController));
+router.put('/updateCliente/:id', autenticacao, clienteController.editarCliente.bind(clienteController));
+router.get('/viewCliente/:id', autenticacao, clienteController.visualizarCliente.bind(clienteController));
+router.get('/desativarCliente/:id', autenticacao, clienteController.desativarCliente.bind(clienteController)); 
 
-export default router
+export default router;
