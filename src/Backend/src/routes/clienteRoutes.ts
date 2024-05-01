749da75044a263e2b208b9b0b4e6d70bd81a71b7
Joyce Silva <142221695+joycesilvaaa@users.noreply.github.com>
2024-05-01 11:47:31 -0300

    feat:[backend] adicionei autenticação nas rotas

--- src/Backend/src/routes/clienteRoutes.ts
+++ src/Backend/src/routes/clienteRoutes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import ClienteController from "../controllers/clienteController";
-import { Autenticacao } from "../controllers/autenticacaoMiddleware";
+import { AuthMiddleware } from "../controllers/authMiddleware";
 
 const router = Router();
 const clienteController = new ClienteController();
-const autenticacao = Autenticacao.autenticacaoToken;
+const autenticacao = AuthMiddleware.authTokenCliente;
 
 router.post('/cadastroCliente', clienteController.cadastrarCliente.bind(clienteController));
 router.post('/logginCliente', clienteController.logginCliente.bind(clienteController));
+router.post('/logoutCliente', clienteController.logoutCliente.bind(clienteController));
 router.put('/updateCliente/:id', autenticacao, clienteController.editarCliente.bind(clienteController));
 router.get('/viewCliente/:id', autenticacao, clienteController.visualizarCliente.bind(clienteController));
 router.get('/desativarCliente/:id', autenticacao, clienteController.desativarCliente.bind(clienteController)); 
--- src/Backend/src/routes/funcionarioRoutes.ts
+++ src/Backend/src/routes/funcionarioRoutes.ts
@@ -1,15 +1,19 @@
 import { Router } from "express";
 import FuncionarioController from "../controllers/funcionarioController";
+import { AuthMiddleware } from "../controllers/authMiddleware";
 
 const router = Router()
 const funcionarioController = new FuncionarioController()
+const authAdmin =  AuthMiddleware.authTokenAdmin
+const authAdminOrAtendente =AuthMiddleware.authTokenAdminOrAtendente
 
-router.post('/cadastroFuncionario', funcionarioController.cadastrarFuncionario.bind(funcionarioController))
-router.put('/updateFuncionario/:id', funcionarioController.editarFuncionario.bind(funcionarioController))
-router.get('/viewFuncionario/:id', funcionarioController.visualizarFuncionario.bind(funcionarioController))
+router.post('/cadastroFuncionario', authAdmin,funcionarioController.cadastrarFuncionario.bind(funcionarioController))
+router.put('/updateFuncionario/:id', authAdmin, funcionarioController.editarFuncionario.bind(funcionarioController))
+router.get('/viewFuncionario/:id', authAdminOrAtendente, funcionarioController.visualizarFuncionario.bind(funcionarioController))
 router.post('/logginFuncionario', funcionarioController.logginFuncionario.bind(funcionarioController))
-router.get('/desativarFuncionario/:id', funcionarioController.desativarFuncionario.bind(funcionarioController))
-router.get('/visualizarTodosFuncionarios', funcionarioController.visualizarTodosFuncionarios.bind(funcionarioController))
+router.get('/logoutFuncionario', funcionarioController.logoutFuncionario.bind(funcionarioController))
+router.get('/desativarFuncionario/:id', authAdmin,funcionarioController.desativarFuncionario.bind(funcionarioController))
+router.get('/visualizarTodosFuncionarios', authAdmin,funcionarioController.visualizarTodosFuncionarios.bind(funcionarioController))
 
 export default router
 
