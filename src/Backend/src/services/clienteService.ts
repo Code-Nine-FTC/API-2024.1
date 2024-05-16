4ce2dc06eadb985d61b3a60d77e3dca64fb6d219
Joyce Silva <142221695+joycesilvaaa@users.noreply.github.com>
2024-05-16 08:43:10 -0300

    fix: [BACKEND] corrigi desativar cliente

--- src/Backend/src/services/clienteService.ts
+++ src/Backend/src/services/clienteService.ts
@@ -3,79 +3,108 @@ import Cliente from "../entities/cliente";
 import { Connection } from "../config/data-source";
 import * as bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken'
+import ChamadoService from "./chamadoService";
 
 export class ClienteService {
+    private clienteRepository = Connection.getRepository(Cliente)
+
+    // Cria novo cliente
     public async cadastrarCliente(dadosCliente: IClienteInput) {
         try {
-            console.log('Recebendo dados no clienteService')
-            console.log(dadosCliente)
-            const clienteRepository = Connection.getRepository(Cliente)
             // Procura se o e-mail ou o cpf já são cadastrados
-            const cliente = await clienteRepository.findOne({
+            const cliente = await this.clienteRepository.findOne({
                 where: [
                     { cli_email: dadosCliente.cli_email },
                     { cli_cpf: dadosCliente.cli_cpf },
                 ]
             })
-            if (!cliente) {
-                // Validação de CPF
-                const cpfValidado = this.validarCpf(dadosCliente.cli_cpf)
-                if (!cpfValidado) {
-                    console.log('CPF invalido')
-                    return { success: false, message: `CPF inválido` }
-                }
-                // Criptografia de Senha
-                const senhaCriptografada = await bcrypt.hash(dadosCliente.cli_senha, 10)
-                dadosCliente.cli_senha = senhaCriptografada
-                // Criando novo usario
-                const novoCliente = clienteRepository.create(dadosCliente)
-                // Salvando novo cliente
-                await clienteRepository.save(novoCliente)
-                return { success: true, message: `Cliente cadastrado com sucesso` }
+            //verifica se o cliente existe e ta desativado
+            if (cliente && !cliente.ativo ) {
+                const senhaCriptografada = await bcrypt.hash(dadosCliente.cli_senha, 10); 
+                const novosDadosCliente = {
+                    cli_senha: senhaCriptografada,
+                    cli_nome: dadosCliente.cli_nome, 
+                    ativo: true 
+                };
+                await this.clienteRepository.update(cliente.cli_id, novosDadosCliente);
+                return { success: true, message: `Cliente ativado com sucesso` };
             }
-            return { success: false, message: `Usuario já cadastrado` }
+            // verifica se ele existe
+            if (cliente) {
+                return { success: false, message: `Usuario já cadastrado` }
+            }
+            // Validação de CPF
+            const cpfValidado = this.validarCpf(dadosCliente.cli_cpf)
+            if (!cpfValidado) {
+                console.log('CPF invalido')
+                return { success: false, message: `CPF inválido` }
+            }
+            // Criptografia de Senha
+            const senhaCriptografada = await bcrypt.hash(dadosCliente.cli_senha, 10)
+            dadosCliente.cli_senha = senhaCriptografada
+            // Criando novo usario
+            const novoCliente = await this.clienteRepository.create(dadosCliente)
+            // Salvando novo cliente
+            await this.clienteRepository.save(novoCliente)
+            return { success: true, message: `Cliente cadastrado com sucesso` }
+
+
 
         } catch (error) {
             console.error(`Erro ao cadastrar cliente:`, error)
             return { success: false, message: `Erro ao cadastrar cliente` }
         }
     }
+
+    // Login do cliente
     public async logginCliente(dadosLogin: IClienteLoggin) {
         try {
+            // Senha para token
             const secret = process.env.SECRET
-            const clienteRepository = Connection.getRepository(Cliente)
-            console.log("Dados recebidos no backend")
-            console.log(dadosLogin)
-            const cliente = await clienteRepository.findOne({ where: { cli_email: dadosLogin.cli_email } })
+            // busca cliente com os dados recebidos
+            const cliente = await this.clienteRepository.findOne({ where: { cli_email: dadosLogin.cli_email } })
+            // verifica se ele existe
             if (!cliente) {
-                console.log("Cliente não encontrado")
                 return { success: false, message: `Cliente não encontrado` }
             }
+            if (cliente.ativo === false) {
+                return { success: false, message: `Cliente Desativado` }
+            }
+            // verifica senha
             if (!bcrypt.compareSync(dadosLogin.cli_senha, cliente.cli_senha)) {
-                console.log("Dados invalidos")
                 return { success: false, message: `Dados invalidos` }
             }
-            const token = jwt.sign({ id: cliente.cli_id, nivelAcesso: 'usuario'}, secret)
-            console.log("Autenticação realizada com sucesso")
-            return { success: true, message: `Autenticação realizada com sucesso`, token,  nivelAcesso: 'usuario' }
+            // cria token  após informações ok
+            const token = jwt.sign({ id: cliente.cli_id, nivelAcesso: 'usuario' }, secret)
+
+            return { success: true, message: `Autenticação realizada com sucesso`, token, nivelAcesso: 'usuario' }
+
         } catch (error) {
             console.error(`Erro ao logar cliente: ${error}`)
-            console.log("Erro ao logar cliente", error)
             return { success: false, message: `Erro ao logar cliente` }
         }
     }
+
+    // Editar cliente
     public async editarCliente(id: number, dadosUpdate: IClienteUpdate) {
         try {
-            const clienteRepository = Connection.getRepository(Cliente)
-            const cliente = await clienteRepository.findOne({ where: { cli_id: id } })
+            // busca cliente
+            const cliente = await this.clienteRepository.findOne({ where: { cli_id: id } })
 
+            // verifica se ele existe
             if (!cliente) {
                 return { success: false, message: `Cliente não encontrado` }
             }
+
+            // verificações 
             if (dadosUpdate.cli_cpf) {
+
                 const cpfValidado = this.validarCpf(dadosUpdate.cli_cpf)
+
                 if (cpfValidado) {
-                    const cpfExistente = await clienteRepository.findOne({ where: { cli_cpf: dadosUpdate.cli_cpf } })
+
+                    const cpfExistente = await this.clienteRepository.findOne({ where: { cli_cpf: dadosUpdate.cli_cpf } })
+
                     if (cpfExistente && cpfExistente.cli_id !== id) {
                         return { success: false, message: `CPF já cadastrado` }
                     }
@@ -83,31 +112,35 @@ export class ClienteService {
                     return { success: false, message: `CPF inválido` }
                 }
             }
+
             if (dadosUpdate.cli_email) {
-                const emailExistente = await clienteRepository.findOne({ where: { cli_email: dadosUpdate.cli_email } })
+                const emailExistente = await this.clienteRepository.findOne({ where: { cli_email: dadosUpdate.cli_email } })
 
                 if (emailExistente && emailExistente.cli_id !== id) {
                     return { success: false, message: `E-mail já cadastrado` }
                 }
             }
+
             if (dadosUpdate.cli_senha) {
                 const senhaCriptografada = await bcrypt.hash(dadosUpdate.cli_senha, 10)
                 dadosUpdate.cli_senha = senhaCriptografada
             }
-
+            // salva de acordo com o que foi alteirado
             const clienteUpdate = { ...cliente, ...dadosUpdate }
-            await clienteRepository.update(id, clienteUpdate)
+            await this.clienteRepository.update(id, clienteUpdate)
             return { success: true, message: `Cliente atualizado com sucesso`, clienteUpdate }
         } catch (error) {
             console.error(`Erro ao editar cliente: ${error}`)
             return { success: false, message: `Erro ao editar cliente` }
         }
     }
+
+    // ver cliente
     public async visualizarCliente(id: number) {
         try {
-            console.log('Recebendo dados em Visualizar Cliente')
-            const clienteRepository = Connection.getRepository(Cliente)
-            const cliente = await clienteRepository.findOne({ where: { cli_id: id } })
+            // busca cliente
+            const cliente = await this.clienteRepository.findOne({ where: { cli_id: id } })
+            // verifica se ele existe
             if (!cliente) {
                 return { success: false, message: `Cliente não encontrado` }
             }
@@ -117,21 +150,31 @@ export class ClienteService {
             return { success: false, message: `Erro ao encontrar o cliente` }
         }
     }
-    public async desativarCliente(id: number) {
+
+    // desativa cliente
+    public async desativarCliente(id: number, chamadoService: ChamadoService) {
         try {
-            const clienteRepository = Connection.getRepository(Cliente)
-            const cliente = await clienteRepository.findOne({ where: { cli_id: id } })
+            // busca cliente
+            const cliente = await this.clienteRepository.findOne({ where: { cli_id: id } })
+            // verifica se ele existe
             if (!cliente) {
-                return { success: false, message: `Cliente não entrado` }
+                return { success: false, message: `Cliente não encontrado` };
             }
-            cliente.ativo = false
-            await clienteRepository.save(cliente)
-            return { success: true, message: `Cliente desativado com sucesso`, cliente }
+            // muda para desativado
+            cliente.ativo = false;
+            // dalva as informações
+            await this.clienteRepository.save(cliente);
+            // chama cancelar chamado do chamado service
+            await chamadoService.cancelarChamado(id);
+
+            return { success: true, message: `Cliente desativado com sucesso` };
         } catch (error) {
-            console.error(`Erro ao encontrar cliente: ${error}`)
-            return { success: false, message: `Erro ao encontrar o cliente` }
+            console.error(`Erro ao desativar cliente: ${error}`);
+            return { success: false, message: `Erro ao desativar cliente` };
         }
     }
+
+    // validação de cpf
     private validarCpf(cpf: string): boolean {
         if (!cpf) {
             console.log('Deu erro na verificação do CPF')
