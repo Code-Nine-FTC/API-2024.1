42519c832ce20261af6e387e3ca742257fef6526
Joyce Silva <142221695+joycesilvaaa@users.noreply.github.com>
2024-05-16 08:38:14 -0300

    feat: [BACKEND] adicionei iniciar chamado, finalizar chamado, chamados em espera

--- src/Backend/src/controllers/chamadoController.ts
+++ src/Backend/src/controllers/chamadoController.ts
@@ -1,19 +1,20 @@
 import { Request, Response } from "express";
 import ChamadoService from "../services/chamadoService";
 import { ClienteService } from "../services/clienteService";
+import { AuthMiddleware } from "./authMiddleware";
 
-class ChamadoController{
+class ChamadoController {
     private chamadoService: ChamadoService
-    constructor(){
-       this.chamadoService = new ChamadoService()
+    constructor() {
+        this.chamadoService = new ChamadoService()
     }
-    
+
     //Para Cliente
-    public async cadastrarChamado(req: Request, res: Response){
-        try{
+    public async cadastrarChamado(req: Request, res: Response) {
+        try {
             // Pega o id passado pela autenticação
             const cli_id = res.locals.userId
-            if( isNaN(cli_id) || cli_id<= 0){
+            if (isNaN(cli_id) || cli_id <= 0) {
                 return res.status(400).json({ success: false, message: `Id do cliente inválido: ID ${cli_id}` });
             }
             // Pega todos os dados enviados pelo req body
@@ -34,200 +35,266 @@ class ChamadoController{
             // envia os dados para chamadoService.cadastrarChamado()
             const resultado = await this.chamadoService.cadastrarChamado(dadosChamado)
 
-            if(!resultado.success){
+            if (!resultado.success) {
                 return res.status(400).json(resultado)
             }
             return res.status(201).json(resultado)
 
-        }catch(error){
+        } catch (error) {
             console.error(`Erro no cadastro do chamado: ${error}`)
             return res.status(500).json({ success: false, message: `Erro interno do Servidor` })
         }
     }
 
-    public async viewChamadosAtivosCliente(req: Request, res: Response){
-        try{
+    public async viewChamadosAtivosCliente(req: Request, res: Response) {
+        try {
             //Pega o id do cliente passado pela autenticação
             const cli_id = res.locals.userId
             // Verificações
-            if( isNaN(cli_id) || cli_id<= 0){
+            if (isNaN(cli_id) || cli_id <= 0) {
                 return res.status(400).json({ success: false, message: `Id do cliente inválido: ID ${cli_id}` });
             }
             // Passa o cli_id para a função no service
             const resultado = await this.chamadoService.visualizarChamadosAtivosCliente(parseInt(cli_id))
-            
-            if(!resultado.success){
+
+            if (!resultado.success) {
                 return res.status(400).json(resultado)
             }
             return res.status(200).json(resultado)
 
-        }catch(error){
+        } catch (error) {
             console.error(`Erro em buscar todos os chamados ativos do cliente: ${error}`)
             return res.status(500).json({ success: false, message: `Erro interno do Servidor` })
         }
     }
 
-    public async viewChamadosFinalizadosCliente(req: Request, res: Response){
-        try{
+    public async viewChamadosFinalizadosCliente(req: Request, res: Response) {
+        try {
             //Pega o id do cliente passado pela autenticação
             const cli_id = res.locals.userId
             // Verificações
-            if( isNaN(cli_id) || cli_id<= 0){
+            if (isNaN(cli_id) || cli_id <= 0) {
                 return res.status(400).json({ success: false, message: `Id do cliente inválido: ID ${cli_id}` });
             }
             // Passa o cli_id para a função no service
             const resultado = await this.chamadoService.visualizarChamadosFinalizadosCliente(parseInt(cli_id))
-            
-            if(!resultado.success){
+
+            if (!resultado.success) {
                 return res.status(400).json(resultado)
             }
             return res.status(200).json(resultado)
 
-        }catch(error){
+        } catch (error) {
             console.error(`Erro em buscar todos os chamados ativos do cliente: ${error}`)
             return res.status(500).json({ success: false, message: `Erro interno do Servidor` })
         }
     }
 
-    public async viewTodosChamadosCliente(req: Request, res: Response){
-        try{
+    public async viewTodosChamadosCliente(req: Request, res: Response) {
+        try {
             //Pega o id do cliente passado pela autenticação
             const cli_id = res.locals.userId
             // Verificações
-            if( isNaN(cli_id) || cli_id<= 0){
+            if (isNaN(cli_id) || cli_id <= 0) {
                 return res.status(400).json({ success: false, message: `Id do cliente inválido: ID ${cli_id}` });
             }
             // Passa o cli_id para a função no service
             const resultado = await this.chamadoService.visualizarTodosChamadosCliente(parseInt(cli_id))
-            
-            if(!resultado.success){
+
+            if (!resultado.success) {
                 return res.status(400).json(resultado)
             }
             return res.status(200).json(resultado)
 
-        }catch(error){
+        } catch (error) {
             console.error(`Erro em buscar todos os chamados ativos do cliente: ${error}`)
             return res.status(500).json({ success: false, message: `Erro interno do Servidor` })
         }
     }
-    
-    public async viewUltimoChamadoCliente(req: Request, res: Response){
-        try{
+
+    public async viewUltimoChamadoCliente(req: Request, res: Response) {
+        try {
             //Pega o id do cliente passado pela autenticação
             const cli_id = res.locals.userId
             // Verificações
-            if( isNaN(cli_id) || cli_id<= 0){
+            if (isNaN(cli_id) || cli_id <= 0) {
                 return res.status(400).json({ success: false, message: `Id do cliente inválido: ID ${cli_id}` });
             }
             // Passa o cli_id para a função no service
             const resultado = await this.chamadoService.visualizarUltimoChamadoCliente(parseInt(cli_id))
-            
-            if(!resultado.success){
+
+            if (!resultado.success) {
                 return res.status(400).json(resultado)
             }
             return res.status(200).json(resultado)
 
-        }catch(error){
+        } catch (error) {
             console.error(`Erro em buscar todos os chamados ativos do cliente: ${error}`)
             return res.status(500).json({ success: false, message: `Erro interno do Servidor` })
         }
     }
 
     //Para Atendente
-    public async viewChamadoEmAtendimentoAtendente(req: Request, res: Response){
-        try{
+    public async viewChamadoEmAtendimentoAtendente(req: Request, res: Response) {
+        try {
             //Pega o id do cliente passado pela autenticação
             const func_id = res.locals.userId
             // Verificações
-            if( isNaN(func_id) || func_id<= 0){
+            if (isNaN(func_id) || func_id <= 0) {
                 return res.status(400).json({ success: false, message: `Id do cliente inválido: ID ${func_id}` });
             }
             // Passa o func_id para a função no service
             const resultado = await this.chamadoService.visualizarChamadoEmAtendimentoAtendente(parseInt(func_id))
-            
-            if(!resultado.success){
+
+            if (!resultado.success) {
                 return res.status(400).json(resultado)
             }
             return res.status(200).json(resultado)
 
-        }catch(error){
+        } catch (error) {
             console.error(`Erro em buscar todos os chamados ativos do cliente: ${error}`)
             return res.status(500).json({ success: false, message: `Erro interno do Servidor` })
         }
     }
 
-    public async viewChamadosFinalizadosAtendente(req: Request, res: Response){
-        try{
+    public async viewChamadosFinalizadosAtendente(req: Request, res: Response) {
+        try {
             //Pega o id do cliente passado pela autenticação
             const func_id = res.locals.userId
             // Verificações
-            if( isNaN(func_id) || func_id<= 0){
+            if (isNaN(func_id) || func_id <= 0) {
                 return res.status(400).json({ success: false, message: `Id do cliente inválido: ID ${func_id}` });
             }
             // Passa o func_id para a função no service
             const resultado = await this.chamadoService.visualizarChamadosFinalizadosAtendente(parseInt(func_id))
-            
+
+            if (!resultado.success) {
+                return res.status(400).json(resultado)
+            }
+            return res.status(200).json(resultado)
+
+        } catch (error) {
+            console.error(`Erro em buscar todos os chamados ativos do cliente: ${error}`)
+            return res.status(500).json({ success: false, message: `Erro interno do Servidor` })
+        }
+    }
+
+    public async iniciarAtendimentoController(req: Request, res: Response) {
+        try {
+            // Pega o id pela url
+            const cha_id = req.params.cha_id
+            console.log(cha_id)
+            // Pega o id passado pela autenticação
+            const func_id = res.locals.userId
+            // Verificações
+            if (isNaN(func_id) || func_id <= 0) {
+                return res.status(400).json({ success: false, message: `Id do cliente inválido: ID ${func_id}` });
+            }
+            // chama função do service
+            const resultado = await this.chamadoService.iniciarAtendimento(parseInt(cha_id), func_id)
+
             if(!resultado.success){
                 return res.status(400).json(resultado)
             }
             return res.status(200).json(resultado)
 
-        }catch(error){
-            console.error(`Erro em buscar todos os chamados ativos do cliente: ${error}`)
-            return res.status(500).json({ success: false, message: `Erro interno do Servidor` })
+        } catch (error) {
+            console.error(`Erro interno do servidor: ${error}`)
+            return res.status(500).json({ success: false, message: `Erro interno do servidor` });
+        }
+    }
+
+    public async finalizarAtendimentoController(req: Request, res: Response) {
+        try {
+            // Pega o id pela url
+            const cha_id = req.params.cha_id
+            // chama função do service
+            const resultado = await this.chamadoService.finalizarAtendimento(parseInt(cha_id))
+
+            if(!resultado.success){
+                return res.status(400).json(resultado)
+            }
+            return res.status(200).json(resultado)
+
+        } catch (error) {
+            console.error(`Erro interno do servidor: ${error}`)
+            return res.status(500).json({ success: false, message: `Erro interno do servidor` });
         }
     }
 
     // Para Administrador
-    public async viewTodosChamadosEmAtendimento(req: Request, res: Response){
-        try{
+    public async viewTodosChamadosEmAtendimento(req: Request, res: Response) {
+        try {
             // Chama a função no service
             const resultado = await this.chamadoService.visualizarTodosChamadosEmAtendimento()
-            
-            if(!resultado.success){
+
+            if (!resultado.success) {
                 return res.status(400).json(resultado)
             }
             return res.status(200).json(resultado)
 
-        }catch(error){
+        } catch (error) {
             console.error(`Erro em buscar todos os chamados ativos do cliente: ${error}`)
             return res.status(500).json({ success: false, message: `Erro interno do Servidor` })
         }
     }
 
-    public async viewTodosChamados(req: Request, res: Response){
-        try{
+    public async viewTodosChamados(req: Request, res: Response) {
+        try {
             // Chama a função no service
             const resultado = await this.chamadoService.visualizarTodosChamados()
-            
+
+            if (!resultado.success) {
+                return res.status(400).json(resultado)
+            }
+            return res.status(200).json(resultado)
+
+        } catch (error) {
+            console.error(`Erro em buscar todos os chamados ativos do cliente: ${error}`)
+            return res.status(500).json({ success: false, message: `Erro interno do Servidor` })
+        }
+    }
+
+    public async direcionaChamadoController(req:Request, res: Response){
+        try{
+            // Pega o id do chamado e do funcionario que foi enviado do corpo
+            const { cha_id, func_id } = req.body
+            // Verificações
+            if (isNaN(func_id) || func_id <= 0) {
+                return res.status(400).json({ success: false, message: `Id do funcionario inválido: ID ${func_id}` });
+            }
+            if (isNaN(cha_id) || cha_id <= 0) {
+                return res.status(400).json({ success: false, message: `Id do chamado inválido: ID ${cha_id}` });
+            }
+            // chama a função do service
+            const resultado = await this.chamadoService.direcionarAtendimento(parseInt(cha_id), parseInt(func_id))
+
             if(!resultado.success){
                 return res.status(400).json(resultado)
             }
             return res.status(200).json(resultado)
 
         }catch(error){
-            console.error(`Erro em buscar todos os chamados ativos do cliente: ${error}`)
-            return res.status(500).json({ success: false, message: `Erro interno do Servidor` })
+            console.error(`Erro interno do servidor: ${error}`)
+            return res.status(500).json({ success: false, message: `Erro interno do servidor` });
+
         }
     }
-
     //Para Ambos (Administrador e Atendente)
-    public async viewChamadosEmEspera(req: Request, res: Response){
-        try{
+    public async viewChamadosEmEspera(req: Request, res: Response) {
+        try {
             // Chama a função no service
             const resultado = await this.chamadoService.visualizarChamadoEmEspera()
-            
-            if(!resultado.success){
+
+            if (!resultado.success) {
                 return res.status(400).json(resultado)
             }
             return res.status(200).json(resultado)
 
-        }catch(error){
+        } catch (error) {
             console.error(`Erro em buscar todos os chamados ativos do cliente: ${error}`)
             return res.status(500).json({ success: false, message: `Erro interno do Servidor` })
         }
     }
-
-
 }
 export default ChamadoController
