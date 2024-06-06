25d226face16ff9fc901ae326f561a83065a9eb0
eduardofsn <eduardonamiuti@hotmail.com>
2024-06-06 11:58:28 -0300

    fix: [Frontend] Listar suporte ajustado

--- src/Frontend/src/pages/listasuporte.tsx
+++ src/Frontend/src/pages/listasuporte.tsx
@@ -13,36 +13,64 @@ const ListagemFuncionarios = () => {
   const [funcionarios, setFuncionarios] = useState<IFuncionarioView[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string>("");
-  const token = getToken()
-  const nivelAcesso = getNivelAcesso()
+  const token = getToken();
+  const nivelAcesso = getNivelAcesso();
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (nivelAcesso !== 'administrador') {
+    if (nivelAcesso!== 'administrador') {
       navigate('/homesup');
       return;
     }
-  
+
     const fetchFuncionarios = async () => {
       try {
         const resultadoListagem = await visualizarTodosFuncionarios();
-        
-        if (resultadoListagem.success){
+
+        if (resultadoListagem.success) {
           setFuncionarios(resultadoListagem.funcionarios);
-          setLoading(false); // Corrigido para false aqui
+          setLoading(false);
           setError('');
         } else {
           setError("Erro ao carregar os dados dos funcionários");
-          setLoading(false); // Corrigido para false aqui também
+          setLoading(false);
         }
       } catch (error) {
         setError("Erro ao carregar os dados dos funcionários");
         setLoading(false);
       }
     };
-  
+
     fetchFuncionarios();
   }, []);
+
+  const columns = [
+    {
+      header: "Nome",
+      accessor: "func_nome"
+    },
+    {
+      header: "CPF",
+      accessor: "func_cpf"
+    },
+    {
+      header: "E-mail",
+      accessor: "func_email"
+    },
+    {
+      header: "Ações",
+      accessor: "func_id",
+      render: (funcionario: { func_id: any; }) => (
+        <Link
+          id={styles.detalheslink}
+          to={`/visualizarfuncionario/${funcionario.func_id}`}
+          style={{ color: 'black'}}
+        >
+          Ver detalhes
+        </Link>
+      )
+    }
+  ];
 
   return (
     <div className={styles.container}>
@@ -51,40 +79,38 @@ const ListagemFuncionarios = () => {
         <h2 className={`${styles.center} ${styles.titleLine}`}>Funcionários Cadastrados</h2>
         {loading && <p>Carregando...</p>}
         {error && <p style={{ color: "red" }}>{error}</p>}
-        <div className={styles.customLayout}>
-          {funcionarios.map((funcionario) => (
-            
-            <div className={styles.funcionarioContainer} key={funcionario.func_id}>
-              <div className={styles.userData}>
-                <h3>Nome</h3>
-                <span>{funcionario.func_nome}</span>
-              </div>
-              <div className={styles.userData}>
-                <h3>CPF</h3>
-                <span>{funcionario.func_cpf}</span>
-              </div>
-              <div className={styles.userData}>
-                <h3>E-mail</h3>
-                <span>{funcionario.func_email}</span>
-              </div>
-              <div className={styles.userData}>
-              <Link
-                id={styles.detalheslink}
-                to={`/visualizarfuncionario/${funcionario.func_id}`}
-                style={{ color: 'black'}}>
-                Ver detalhes
-              </Link>
-              </div>
-            </div>
-          ))}
-    </div>
-          <div className={styles.buttonContainer}>
-            <Link to="/registrosuporte"> <button type="button">Cadastrar Atendente</button></Link>
-          </div>
-       
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              {columns.map((column) => (
+                <th key={column.header} scope="col">{column.header}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {funcionarios.map((funcionario) => (
+              <tr key={funcionario.func_id}>
+                <td>{funcionario.func_nome}</td>
+                <td>{funcionario.func_cpf}</td>
+                <td>{funcionario.func_email}</td>
+                <td>
+                  <Link
+                    id={styles.detalheslink}
+                    to={`/visualizarfuncionario/${funcionario.func_id}`}
+                    style={{ color: 'black'}}
+                  >
+                    Ver detalhes
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className={styles.buttonContainer}>
+          <Link to="/registrosuporte"> <button type="button">Cadastrar Atendente</button></Link>
+        </div>
       </div>
     </div>
-    
   );
 };
 
